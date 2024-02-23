45bea54a2a7a245a3ecd6ccd235a49fec60b801a
solid0x <solidz0x@gmail.com>
2024-02-23 22:54:09 +0700

    Refactor test.sh

--- app/test.sh
+++ app/test.sh
@@ -1,1 +1,2 @@
-DATABASE_URL="sqlite+aiosqlite:///:memory:" pytest -p no:cacheprovider --cov=src --cov-report=html
+export DATABASE_URL="sqlite+aiosqlite:///:memory:"
+pytest -p no:cacheprovider --cov=src --cov-report=html
